392a33d77c341982ed0100730249ee2500433a5c
anil-allipilli <anilkmr458@gmail.com>
2020-11-09 03:37:43 +0530

    sponsee model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+def academic_level_choices():
+    return [(r, r) for r in range(1, 13)]
+
+
+def year_choices():
+    return [(r, r) for r in range(datetime.date.today().year+1, datetime.date.today().year+13)]
+
+
+class Sponsee(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    address = models.TextField(max_length=512)
+    phone = models.CASCADE(max_length=12)
+    birth_certificate = models.FileField(upload_to="birth_certificate/")
+    national_id = models.FileField(upload_to="national_id/")
+
+    def clean(self):
+        try:
+            phonenumbers.parse(self.phone, None)
+        except phonenumbers.phonenumberutil.NumberParseException:
+            raise ValidationError(
+                _("Invalid phone number")
+            )
+
+    def __str__(self):
+        return self.user.username
--- sponsor/settings.py
+++ sponsor/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$a02n7w*^!)9mo4c(^&%3-w-lku_q=0o1p!*@#xp*n^m@)1c9)'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
